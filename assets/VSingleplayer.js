92e9d92c8c786c8b619e2fc78317d098db86ea6a
awehero <147273991+awehero@users.noreply.github.com>
2024-12-01 19:43:52 -0500

    Update VSingleplayer.js

--- assets/VSingleplayer.js
+++ assets/VSingleplayer.js
@@ -223,7 +223,7 @@ class SkinUtils {
     if (skinId > 57) {
         return JSON.parse(localStorage.getItem("CupImages"))[skinId-58]
     }
-    else {return `/assets/skins/${skinName}.png`;}
+    else {return `/DodoLoader/assets/skins/${skinName}.png`;}
   }
   static getPercent(progress) {
     return progress.completedMaps / progress.totalMaps;
@@ -1032,7 +1032,7 @@ class FSinglePopupManager {
                     <li>Turn on "Use hardware acceleration when available"</li>
                     <li>Click the "Relaunch" button</li>
                 </ul>
-                <img src="/assets/other/engine_error.gif" style="width: 100%" />
+                <img src="/DodoLoader/assets/other/engine_error.gif" style="width: 100%" />
             `,
       buttons: [{
         text: CLOSE_BUTTON_TEXT,
@@ -1223,7 +1223,7 @@ class FSinglePopupManager {
                 Use the arrow keys to move.<br>dfskhklfasdilfisdahfl
                 Hit the green portal to win.<br>
                 Do not fall off or hit the spikes.<br>
-                <img style="width: 140px;" src="/assets/icons/controls.png" alt="controls">
+                <img style="width: 140px;" src="/DodoLoader/assets/icons/controls.png" alt="controls">
                 <br><br>
                 In-game keyboard shortcuts:
                 <ul>
@@ -14447,12 +14447,12 @@ class FBaseSoundPlayer {
     const isSoundOn = await FStorage.getString(StorageKeyEnum.IsSoundOn);
     this.isSoundOn = isSoundOn === StorageValueEnum.On;
     const svgName = this.isSoundOn ? "sound-on.svg" : "sound-off.svg";
-    this.overlayToggleSoundImg.src = "/assets/svgs/" + svgName;
+    this.overlayToggleSoundImg.src = "/DodoLoader/assets/svgs/" + svgName;
   }
   async createBackgroundMusic() {
     const musicType = await FStorage.getString(StorageKeyEnum.SelectedSoundtrack);
     return A.from({
-      url: "/assets/" + this.getSoundTrackSelected(musicType ?? StorageValueEnum.MusicDefault),
+      url: "/DodoLoader/assets/" + this.getSoundTrackSelected(musicType ?? StorageValueEnum.MusicDefault),
       autoPlay: false,
       loop: true,
       volume: 0.35,
@@ -14462,7 +14462,7 @@ class FBaseSoundPlayer {
   }
   createMenuMusic() {
     return A.from({
-      url: "/assets/" + SoundFileEnum.Brink,
+      url: "/DodoLoader/assets/" + SoundFileEnum.Brink,
       autoPlay: false,
       loop: true,
       volume: 0.2,
@@ -14471,7 +14471,7 @@ class FBaseSoundPlayer {
     });
   }
   loadSound(audioUrl) {
-    const link = "/assets/" + audioUrl;
+    const link = "/DodoLoader/assets/" + audioUrl;
     return A.from(link);
   }
   getSoundTrackSelected(Music) {
@@ -14820,15 +14820,15 @@ const _sfc_main$e = defineComponent({
     }
   }
 });
-const _imports_0$4 = "/assets/svgs/fullscreen.svg";
-const _imports_0$3 = "/assets/svgs/sound-on.svg";
-const _imports_2$2 = "/assets/svgs/arrow-left.svg";
-const _imports_1$2 = "/assets/svgs/jump_enabled.svg";
-const _imports_1$3 = "/assets/svgs/arrow-left.svg";
-const _imports_2$1 = "/assets/svgs/controls_reversed.svg";
-const _imports_3$1 = "/assets/svgs/drift_enabled.svg";
-const _imports_7$1 = "/assets/svgs/help.svg";
-const _imports_7 = "/assets/svgs/info.svg";
+const _imports_0$4 = "/DodoLoader/assets/svgs/fullscreen.svg";
+const _imports_0$3 = "/DodoLoader/assets/svgs/sound-on.svg";
+const _imports_2$2 = "/DodoLoader/assets/svgs/arrow-left.svg";
+const _imports_1$2 = "/DodoLoader/assets/svgs/jump_enabled.svg";
+const _imports_1$3 = "/DodoLoader/assets/svgs/arrow-left.svg";
+const _imports_2$1 = "/DodoLoader/assets/svgs/controls_reversed.svg";
+const _imports_3$1 = "/DodoLoader/assets/svgs/drift_enabled.svg";
+const _imports_7$1 = "/DodoLoader/assets/svgs/help.svg";
+const _imports_7 = "/DodoLoader/assets/svgs/info.svg";
 const VGame_vue_vue_type_style_index_0_scoped_31fa11aa_lang = "";
 const _withScopeId$9 = (n2) => (pushScopeId("data-v-31fa11aa"), n2 = n2(), popScopeId(), n2);
 const _hoisted_1$e = { id: "gameMain" };
@@ -15000,7 +15000,7 @@ const _sfc_main$c = defineComponent({
     this.isStickyVisible = true;
   }
 });
-const _imports_0$2 = "/assets/svgs/cross.svg";
+const _imports_0$2 = "/DodoLoader/assets/svgs/cross.svg";
 const VSticky_vue_vue_type_style_index_0_scoped_92828c32_lang = "";
 const _hoisted_1$c = {
   key: 0,
@@ -15175,15 +15175,15 @@ const _sfc_main$a = defineComponent({
   },
   components: { VMapListing, VSticky, VBrewListing }
 });
-const _imports_0$1 = "/assets/svgs/skins.svg";
-const _imports_1$1 = "/assets/svgs/finder.svg";
-const _imports_2 = "/assets/svgs/settings.svg";
-const _imports_3 = "/assets/svgs/multiplayer.svg";
-const _imports_4 = "/assets/svgs/sync.svg";
-const _imports_5 = "/assets/svgs/caret-left-fill.svg";
-const _imports_6 = "/assets/svgs/caret-right-fill.svg";
-const _imports_8 = "/assets/icons/discord.svg";
-const _imports_9 = "/assets/icons/instagram.svg";
+const _imports_0$1 = "/DodoLoader/assets/svgs/skins.svg";
+const _imports_1$1 = "/DodoLoader/assets/svgs/finder.svg";
+const _imports_2 = "/DodoLoader/assets/svgs/settings.svg";
+const _imports_3 = "/DodoLoader/assets/svgs/multiplayer.svg";
+const _imports_4 = "/DodoLoader/assets/svgs/sync.svg";
+const _imports_5 = "/DodoLoader/assets/svgs/caret-left-fill.svg";
+const _imports_6 = "/DodoLoader/assets/svgs/caret-right-fill.svg";
+const _imports_8 = "/DodoLoader/assets/icons/discord.svg";
+const _imports_9 = "/DodoLoader/assets/icons/instagram.svg";
 const VMenu_vue_vue_type_style_index_0_scoped_c2338e83_lang = "";
 const _withScopeId$6 = (n2) => (pushScopeId("data-v-c2338e83"), n2 = n2(), popScopeId(), n2);
 const _hoisted_1$a = { class: "menuMain" };
@@ -15460,7 +15460,7 @@ const _sfc_main$9 = defineComponent({
   mounted() {
   }
 });
-const _imports_0 = "/assets/svgs/back_arrow.svg";
+const _imports_0 = "/DodoLoader/assets/svgs/back_arrow.svg";
 const VCredits_vue_vue_type_style_index_0_scoped_c7113168_lang = "";
 const _withScopeId$5 = (n2) => (pushScopeId("data-v-c7113168"), n2 = n2(), popScopeId(), n2);
 const _hoisted_1$9 = { class: "titleWithIcon" };
@@ -16125,7 +16125,7 @@ const _sfc_main$2 = defineComponent({
       this.selectedSkinId = selectedSkinId;
   }
 });
-const _imports_1 = "/assets/svgs/lock.svg";
+const _imports_1 = "/DodoLoader/assets/svgs/lock.svg";
 const VSkins_vue_vue_type_style_index_0_scoped_1a1c1d8c_lang = "";
 const _withScopeId = (n2) => (pushScopeId("data-v-1a1c1d8c"), n2 = n2(), popScopeId(), n2);
 const _hoisted_1$2 = { class: "titleWithIcon" };
